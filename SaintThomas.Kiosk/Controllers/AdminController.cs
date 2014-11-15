6f89250e77e2410845b19dc5cead694b3942773e
Keith Kikta <kkikta@gmail.com>
2014-11-15 11:41:46 -0600

    add start of administration

---
+++ SaintThomas.Kiosk/Controllers/AdminController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaintThomas.Kiosk.Controllers
+{
+    public class AdminController : RavenController
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var users = RavenSession.Query<Models.ApplicationUser>().ToList();
+            
+            return View(users);
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
+        {
+            var user = RavenSession.Query<Models.ApplicationUser>().Where(x => x.Id = id);
+            return View(user);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
