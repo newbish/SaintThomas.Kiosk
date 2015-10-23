7ee1065e5a3673c0f5d5b22183e1a2f704c5867d
Keith Kikta <kkikta@gmail.com>
2015-10-22 22:29:43 -0500

    Added setup feature

---
+++ README.md
@@ -0,0 +1,13 @@
+#SaintThomas.Kiosk 
+
+This application is a web based kiosk capable of displaying informational 
+videos. It uses an embeded RavenDB database so that it can be self 
+contained on the machine running it. Its designed primarily to be used on
+touch screen devices.
+
+##Setup
+When first setting up the kiosk use the setup page to set the inital login.
+Once setup you can add images and link to videos that support oembed 
+(youtube, vine, instagram, etc.)
+Username: admin@stthom.edu
+Password: admin123
---
+++ SaintThomas.Kiosk/Controllers/SetupController.cs
@@ -0,0 +1,52 @@
+﻿using Raven.Client;
+using SaintThomas.Kiosk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RavenDB.AspNet.Identity;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SaintThomas.Kiosk.Controllers
+{
+    public class SetupController : RavenController
+    {
+        public SetupController()
+        {
+            var ustore = new UserStore<ApplicationUser>(() => this.RavenSession);
+            this.UserManager = new UserManager<ApplicationUser>(ustore);
+        }
+
+        public SetupController(UserManager<ApplicationUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserManager<ApplicationUser> UserManager { get; private set; }
+
+        public async Task<ActionResult> Index()
+        {
+            RavenQueryStatistics stats;
+            var users = RavenSession.Query<Models.ApplicationUser>()
+                .Statistics(out stats)
+                .ToList();
+            if (users.Count != 0)
+                return RedirectToAction("Index", "Home");
+            var model = new {Email = "admin@stthom.edu", Password = "admin123"};
+            var user = new ApplicationUser {UserName = model.Email, Email = model.Email};
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var roleClaim = new Claim(ClaimTypes.Role, "User");
+                await UserManager.AddClaimAsync(user.Id, roleClaim);
+                roleClaim = new Claim(ClaimTypes.Role, "Admin");
+                await UserManager.AddClaimAsync(user.Id, roleClaim);
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
