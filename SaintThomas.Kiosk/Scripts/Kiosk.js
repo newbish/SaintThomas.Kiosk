5359b1e75ae118b6031ac7861094aad57232da7d
Keith Kikta <kkikta@gmail.com>
2015-10-27 00:52:26 -0500

    added full screen mode and fixed issues with deployment

--- SaintThomas.Kiosk/Scripts/Kiosk.js
+++ SaintThomas.Kiosk/Scripts/Kiosk.js
@@ -56,3 +56,31 @@ function onYouTubePlayerAPIReady() {
         videoId: 'M7lc1UVf-VE'
     });
 }
+
+function DoFullScreen() {
+
+    var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) || // alternative standard method  
+    (document.mozFullScreen || document.webkitIsFullScreen);
+
+    var docElm = document.documentElement;
+    if (!isInFullScreen) {
+
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        } else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+            alert("Mozilla entering fullscreen!");
+        } else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+            alert("Webkit entering fullscreen!");
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
